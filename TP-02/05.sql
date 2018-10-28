d7a79832d3e1dad58ec75d44a5f801861dacfc65
Iván Fretes <vanfretes@gmail.com>
2018-10-28 19:16:35 -0300

    ejercicio 5

--- TP-02/05.sql
+++ TP-02/05.sql
@@ -1,11 +1,14 @@
+/**
+	Ejercicio 5
 
--- Ejercicio 5
+	Cree el trigger T_VERIFICAR_RESERVA que deberá dispararse antes de  INSERTAR un registro en la tabla RESERVA_TRAMO.  El trigger deberá determinar con la función F_DETERMINAR_DISPONIBILIDAD del paquete PCK_VUELO, la cantidad de asientos libres para el vuelo y fecha. Si la reserva a incorporar supera la capacidad, debe impedir la inserción. 
+**/
+
 CREATE OR REPLACE TRIGGER T_VERIFICAR_RESERVA 
 BEFORE INSERT ON RESERVA_TRAMO
 DECLARE
 	V_CANT_DISPONIBLES 	NUMBER;
 BEGIN
-
 	V_CANT_DISPONIBLES := PKG_VUELO.F_DETERMINAR_DISPONIBILIDAD(
 		:new.ID_ESCALA, 
 		:new.FECHA_SALIDA_PROGRAMADA, 
@@ -14,6 +17,6 @@ BEGIN
 
 	-- Si no hay asientos disponibles
 	IF V_CANT_DISPONIBLES == 0 THEN
-		DBMS_OUT.PUT_LINE('No hay asientos disponibles');
+		RAISE_APPLICATION_ERROR(-20002, 'No hay asientos disponibles');
 	END IF;
 END;
--- TP-02/06.sql
+++ TP-02/06.sql
@@ -1,6 +1,18 @@
+/**
+	
+	Ejercicio 6
+	
+	Cree el trigger T_VERIFICAR_TRIPULACION que se deberá disparar antes de INSERTAR en la tabla TRIPULACIÓN, el cual deberá controlar lo siguiente: Se debe  impedir que se incorpore a la tripulación de un vuelo, una persona que ya tiene expirada su patente. 
+
+	Por cada realización de vuelo puede haber un solo comandante.
+	Sólo puede agregarse un miembro a la tripulación de un vuelo confirmado. En cualquier otro estado debe rechazarse la modificación.
+**/
+
+
 CREATE OR REPLACE TRIGGER T_VERIFICAR_TRIPULACION 
 BEFORE INSERT ON tripulacion
 DECLARE
 BEGIN
-	...
+	
+
 END;
